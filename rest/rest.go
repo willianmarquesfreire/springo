2ae266571330b1226a6bd07b8fc42f3fd745954c
wmfsystem <willianmarquesfreire@gmail.com>
2018-02-01 21:23:27 -0200

    testes, logs

---
+++ rest/rest.go
@@ -0,0 +1,191 @@
+package rest
+
+import (
+	"regexp"
+	"net/http"
+	"reflect"
+	"encoding/json"
+	"strings"
+)
+
+/**
+	API - Application Programming Interface
+ */
+type Api struct {
+	resources map[string][]Resource
+	Addr      string
+	NotFound  interface{}
+	BaseUrl   string
+}
+
+/**
+	Inicializa configurações iniciais da API
+ */
+func (api Api) NewMux() Api {
+	api.resources = make(map[string][]Resource)
+	return api
+}
+
+/**
+	Inicializa a API de acordo com as configurações realizadas
+ */
+func (api Api) ListenAndServe() Api {
+
+	http.HandleFunc(api.BaseUrl, func(ww http.ResponseWriter, rr *http.Request) {
+		respBody := MountResponseJSON(ww, rr, api)
+		//ww.Header().Set("Content-Type", "application/json; charset=utf-8")
+		if reflect.TypeOf(respBody).String() != "[]uint8" {
+			respBody, _ = json.MarshalIndent(respBody, "", "  ")
+			ww.Write(respBody.([]byte))
+		} else {
+			ww.Write(respBody.([]uint8))
+		}
+	})
+
+	http.ListenAndServe(api.Addr, nil)
+	return api
+}
+
+/**
+	Monta JSON de resposta
+ */
+func MountResponseJSON(ww http.ResponseWriter, rr *http.Request, api Api) interface{} {
+	request := &Request{}
+	request.Request = rr
+
+	response := Response{}
+	response.ResponseWriter = ww
+
+	args := MonteArgsListAccordingParamsToCallMethod(response, request)
+	resource := GetResourceOnApiOfRequest(api, request)
+	request.PathVariables = GetPathVariablesOfRequestOnResource(request, resource)
+	values := GetReturningValuesOfCallMethodOfResourceAccordingParams(resource, request, args, api)
+
+	respBody := MountRespBodyAccordingValues(values)
+	return respBody
+}
+
+/**
+	Função que monta um json de acordo com a lista de valores. Utilizada na função MountResponseJSON
+ */
+func MountRespBodyAccordingValues(values []reflect.Value) interface{} {
+	var respBody interface{}
+	if values != nil && len(values) > 0 {
+		respBody = values[0].Interface()
+	} else {
+		respBody, _ = json.MarshalIndent(values, "", "  ")
+	}
+	return respBody
+}
+
+/**
+	Verifica método da requisição com do recurso utilizado, e chama a função do recurso com determinados argumentos. Se o método do recurso for diferente da requisição, é retornado o método NotFound da api
+ */
+func GetReturningValuesOfCallMethodOfResourceAccordingParams(resource Resource, rr *Request, args []reflect.Value, api Api) []reflect.Value {
+	var values []reflect.Value
+	if resource.Method == rr.Method {
+		values = reflect.ValueOf(resource.Function).Call(args)
+	} else {
+		values = reflect.ValueOf(api.NotFound).Call(args)
+	}
+	return values
+}
+
+/**
+	Monta lista de argumentos que serão utilizados no método GetReturningValuesOfCallMethodOfResourceAccordingParams
+ */
+func MonteArgsListAccordingParamsToCallMethod(response Response, request *Request) []reflect.Value {
+	args := make([]reflect.Value, 2)
+	args[0] = reflect.ValueOf(response)
+	args[1] = reflect.ValueOf(request)
+	return args
+}
+
+/**
+	Retorna Lista de variáveis de URL de acordo com a requisição e recurso
+ */
+func GetPathVariablesOfRequestOnResource(rr *Request, resource Resource) map[string]interface{} {
+	splitUrl := strings.Split(rr.URL.Path, "/")
+	aux := make(map[string]interface{})
+	for key, element := range resource.Info.PathVariables {
+		aux[key] = splitUrl[element]
+	}
+	return aux
+}
+
+/**
+	Retorna recurso da Api utilizado na requisição
+ */
+func GetResourceOnApiOfRequest(api Api, rr *Request) Resource {
+	rs := api.resources[strings.Split(rr.URL.Path, "/")[0]]
+	var r Resource
+	for _, element := range rs {
+		if len(element.Info.Regex.FindString(rr.URL.Path)) > 0 {
+			r = element
+			break
+		}
+	}
+	return r
+}
+
+/**
+	Função executada quando ouver algum erro no mapeamento de determinada rota
+ */
+func (api *Api) OnNotFound(fnNotFound interface{}) *Api {
+	api.NotFound = fnNotFound
+	return api
+}
+
+/**
+	Registra Recurso da API
+ */
+func (api *Api) Register(r Resource) *Api {
+	bars := strings.Split(r.Path, "/")
+	r.Info = ResourceInfo{}
+	r.Info.PathVariables = make(map[string]int)
+
+	for i, elem := range bars {
+		if len(regexp.MustCompile("{\\w+}").FindString(elem)) > 0 {
+			r.Info.PathVariables[regexp.MustCompile("{|}").ReplaceAllString(elem, "")] = i
+		}
+	}
+
+	r.Info.Regex, _ = regexp.Compile("^" + regexp.MustCompile("{\\w+}").ReplaceAllString(r.Path, "(\\w+)") + "$")
+	pos := strings.Split(r.Path, "/")[0]
+	api.resources[pos] = append(api.resources[pos], r)
+
+	return api
+}
+
+/**
+	Informações adicionais do recurso
+ */
+type ResourceInfo struct {
+	PathVariables map[string]int
+	Regex         *regexp.Regexp
+}
+
+/**
+	Recurso da API
+ */
+type Resource struct {
+	Path     string
+	Method   string
+	Function interface{}
+	Info     ResourceInfo
+}
+
+/**
+	Requisição
+ */
+type Request struct {
+	*http.Request
+	PathVariables map[string]interface{}
+}
+
+/**
+	Resposta da requisição
+ */
+type Response struct {
+	http.ResponseWriter
+}
---
+++ rest/test/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"os"
+	"log"
+	"io/ioutil"
+	"net/http"
+	"springo/rest"
+	"springo/config"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("ola")
+	config.StartConfigurationTestMock()
+	api := rest.Api{Addr: ":8086", BaseUrl: "/"}.NewMux()
+	api.OnNotFound(NotFound)
+	api.Register(rest.Resource{Path: "/testando/{nome}", Method: "GET", Function: Testando})
+	api.Register(rest.Resource{Path: "/testando1/\\d", Method: "GET", Function: Testando1})
+	api.Register(rest.Resource{Path: "/testando2", Method: "GET", Function: Testando2})
+	api.Register(rest.Resource{Path: "/testando3", Method: "GET", Function: Testando3})
+	api.Register(rest.Resource{Path: "/testando4", Method: "GET", Function: Testando4})
+	api.ListenAndServe()
+}
+
+type Pessoa struct {
+	Nome string `json:"nome"`
+}
+
+type Endereco struct {
+	Rua string `json:"rua"`
+}
+
+func Testando(w rest.Response, r *rest.Request) Pessoa {
+	a := r.PathVariables["nome"]
+	return Pessoa{Nome: a.(string)}
+}
+
+func Testando1(w rest.Response, r *rest.Request) string {
+	return "Willian"
+}
+
+
+func Testando2(w rest.Response, r *rest.Request) Endereco {
+	return Endereco{Rua: "Rua Willian"}
+}
+
+func Testando3(w rest.Response, r *rest.Request) []byte {
+	img, err := os.Open("./teste.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer img.Close()
+	w.Header().Set("Content-Type", "image/jpeg")
+	a, _ := ioutil.ReadAll(img)
+	return a
+
+}
+
+func Testando4(w rest.Response, r *rest.Request) []byte {
+	file, err := os.Open("./teste.zip")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	w.Header().Set("Content-Type", "multipart/form-data;")
+	a, _ := ioutil.ReadAll(file)
+	return a
+}
+
+func NotFound(w http.ResponseWriter, r *rest.Request) string {
+	return "Rota não encontrada"
+}
+
